a1d4f1049f32831b8ffb1ec4e2739cf18f2b25e7
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2023-05-15 21:49:33 -0300

    exercicio_gabrielfs

---
+++ exerc1.py
@@ -0,0 +1,8 @@
+# Solicita um número ao usuário
+numero = int(input("Digite um número: "))
+
+# Verifica se o número é par ou ímpar
+if numero % 2 == 0:
+    print("O número", numero, "é par.")
+else:
+    print("O número", numero, "é ímpar.")
